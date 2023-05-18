85da99b64ef427d668e2fee30118572d135ff553
Andy Pierce <spudos16@gmail.com>
2023-05-18 17:02:49 +0100

    added table view and tidied up layout

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,10 @@
 class Stock < ApplicationRecord
 
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
+
 def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:pub_api_key],
                 endpoint: 'https://cloud.iexapis.com/v1')
